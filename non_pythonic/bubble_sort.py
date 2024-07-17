aabb647fe5ea8d952320f36b2671f167b5bd2472
Julian <julian.nicholas.weidner@gmail.com>
2024-07-17 18:43:44 +0200

    cleaned up a bit more

--- non_pythonic/bubble_sort.py
+++ non_pythonic/bubble_sort.py
@@ -3,10 +3,11 @@ given_list =  [64, 34, 25, 12, 22, 11, 90]
 
 
 def bbl_sort(given, reverse=False) -> list:
-    for index in range(len(given)):
+    max_index = len(given) - 1
+    for _ in range(max_index): #when not using the value  just use ' _'
         i = 0
         k = 1
-        while k <= len(given) - 1:
+        while k <= max_index:
             #ascending
             if reverse == False: 
                 if given[i] > given[k]:
@@ -22,6 +23,6 @@ def bbl_sort(given, reverse=False) -> list:
             i+=1 
             k+=1
     return given
-print(bbl_sort(given_list, True))
-print(bbl_sort(given_list, False))
+print(bbl_sort(given_list[:], False)) #since this is modifiing the original list, I could use given_list[:] to create a shallow list (a copy). Shouldn't matter in this instance, but would be good practice.
+print(bbl_sort(given_list[:], True))
 
