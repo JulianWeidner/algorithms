be645305a5d84750fb3bbc70be16307b4551750c
Julian <julian.nicholas.weidner@gmail.com>
2024-07-17 18:14:47 +0200

    cleaned up

--- non_pythonic/bubble_sort.py
+++ non_pythonic/bubble_sort.py
@@ -3,43 +3,25 @@ given_list =  [64, 34, 25, 12, 22, 11, 90]
 
 
 def bbl_sort(given, reverse=False) -> list:
-    i = 0
-    k = 1
     for index in range(len(given)):
         i = 0
         k = 1
-        if reverse == False:
-            while k <= len(given) - 1:
+        while k <= len(given) - 1:
+            #ascending
+            if reverse == False: 
                 if given[i] > given[k]:
                     tmp = given[i]
                     given[i] = given[k]
                     given[k] = tmp
-                i+=1 
-                k+=1
-        if reverse== True:
-            while k <= len(given) - 1:
-                if given[i] < given[k]:
-                    tmp = given[i]
-                    given[i] = given[k]
-                    given[k] = tmp
-                i+=1 
-                k+=1
-                    
-
-
+            elif reverse == True:
+                    #descending
+                    if given[i] < given[k]:
+                        tmp = given[i]
+                        given[i] = given[k]
+                        given[k] = tmp
+            i+=1 
+            k+=1
     return given
-    """
-         elif reverse == True:
-            while k <= len(given) - 1:
-                if given[i] < given[k]:
-                    tmp = given[k]
-                    given[k] = given[i]
-                    given[i] = tmp
-                i+=1 
-                k+=1
-        return given
-        """
-       
-    
 print(bbl_sort(given_list, True))
+print(bbl_sort(given_list, False))
 
